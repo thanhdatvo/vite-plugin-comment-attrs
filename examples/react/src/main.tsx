cc5efa2c0aee34fc12d706bbf931a67b2aa2a204
Thanh Dat Vo <vo9312@gmail.com>
2026-05-09 15:14:10 -0700

    update example

--- examples/react/src/main.tsx
+++ examples/react/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.after.tsx";
+// import App from "./App.before.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
